4a05932990e6dae1af5620efe8dd3e901f5ed90b
zzz <zzz@163.com>
2019-07-23 15:55:06 +0800

    ceshi

--- src/main/java/com/qf/controller/MyController.java
+++ src/main/java/com/qf/controller/MyController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 @Controller
@@ -27,11 +28,11 @@ public class MyController {
             byte[] b = file.getBytes();
             FastDFSUtils fastDFSUtils = new FastDFSUtils();
             String[] s = fastDFSUtils.upload(b, suffix);
-            StringBuilder stringBuilder=new StringBuilder("http://192.168.164.88:82/");
-            if (s!=null){
+            StringBuilder stringBuilder = new StringBuilder("http://192.168.164.88:82/");
+            if (s != null) {
                 for (int i = 0; i < s.length; i++) {
                     stringBuilder.append(s[i]);
-                    if (i==0){
+                    if (i == 0) {
                         stringBuilder.append("/");
                     }
                 }
@@ -47,7 +48,36 @@ public class MyController {
 
     @RequestMapping("/fileupload.json")
     @ResponseBody
-    public Map<String,Object> findall(){
-        return null;
+    public Map fileupload(@RequestParam("file") MultipartFile file) {
+        Map map = new HashMap();
+        try {
+            //1.得到名称
+            String name = file.getOriginalFilename();
+            //2.得到后缀
+            String suffix = name.substring(name.lastIndexOf(".") + 1);
+            //3.得到上传文件的数组
+            byte[] b = file.getBytes();
+            FastDFSUtils fastDFSUtils = new FastDFSUtils();
+            String[] s = fastDFSUtils.upload(b, suffix);
+            StringBuilder stringBuilder = new StringBuilder("http://192.168.164.88:82/");
+            if (s != null) {
+                for (int i = 0; i < s.length; i++) {
+                    stringBuilder.append(s[i]);
+                    if (i == 0) {
+                        stringBuilder.append("/");
+                    }
+                }
+            }
+            String url = stringBuilder.toString();
+            map.put("status", 200);
+            map.put("msg", "success");
+            map.put("url", url);
+            return map;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        map.put("status", 500);
+        map.put("msg", "file");
+        return map;
     }
 }
